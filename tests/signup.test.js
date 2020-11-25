750feb58263795b350a9e43b6096a45fed86e8b5
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2020-11-25 12:27:41 +0100

    jest

--- tests/signup.test.js
+++ tests/signup.test.js
@@ -4,22 +4,32 @@ var mongoose = require("mongoose")
 
 var User = require("../bdd/SchemaHote")
 
-beforeAll(async () => {
-  const url = `mongodb+srv://admin/30094561@cluster0.xutoc.mongodb.net/EveryOneIsTheDJ?retryWrites=true&w=majority`
-  await mongoose.connect(url, { useNewUrlParser: true })
-})
+/**
+ * Connexion à la mémoire de la base de données avant tout test
+ */
+beforeAll(async () => await dbHandler.connect());
+/**
+ * Nettoyage de tout test avant de procéder à un test
+ */
+afterEach(async () => await dbHandler.clearDatabase());
+/**
+ * Fermeture de la base de donnée et du serveur
+ */
+afterAll(async () => await dbHandler.closeDatabase());
+/**
+ * Début du test
+ */
+describe('signup', () => {
+  it('signup fonctionne', async () => {
 
-test("timer", async (done) => {
   var test = await request(app).post("/sign-up")
-
   .send({
     "name": 'f',
     "email": 'f'
   })
-
     expect(user.body.name).toBeTruthy()
     expect(user.body.email).toBeTruthy()
-
   done()
   
   })
+});
